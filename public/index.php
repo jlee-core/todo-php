06a8d90fa3658bcbef0ed96e259b90ecbd4aca55
j.lee <j.lee@core-tech.jp>
2026-04-20 09:34:42 +0900

    変数修正とbugfix
    - webの表示が一個遅れるバグを修正
    - todoを追加した後にheader('Location: index.php')を実行するように修正

--- public/index.php
+++ public/index.php
@@ -1,40 +1,21 @@
 <?php
+require __DIR__ . '/../app/controller/TodoController.php';
+
 // jsonファイルのパスを指定
 $filePath = '../todos.json';
 
-// 配列宣言
-$todoList = [];
+$todoController = new TodoController();
 
-// 存在確認
-if (file_exists($filePath)) {
-  // json変数に読み込み
-  $json = file_get_contents($filePath);
-  // todoList配列に格納, falseの場合は空配列
-  $todoList = json_decode($json, true) ?? [];
-}
+// 配列を更新する
+$todos = $todoController->getTodos($filePath);
 
-//　空白文字を除去、空文字チェック
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $title = trim($_POST['title'] ?? '');
+// Todoを追加する関数
+$todoController->addTodo($filePath, $todos);
 
-  //空文字ではない場合,新しTodoを作成
-  if ($title !== '') {
-    $newTodo = [
-      'id' => count($todoList) + 1,
-      'title' => $title,
-      'created_at' => date('c')
-    ];
+$deleteId = -1;
+//　Todoを削除する関数
+$todoController->deleteTodo($filePath, $todos, $deleteId);
 
-    // 配列に追加
-    $todoList[] = $newTodo;
-
-    // 配列をjson形式に変換して保存
-    file_put_contents(
-      $filePath,
-      json_encode($todoList, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
-    );
-  }
-}
 ?>
 
 <!DOCTYPE html>
@@ -48,23 +29,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
   <h1>Todoアプリ</h1>
-
-  <ul>
-
-  </ul>
-
   <form method="POST" action="">
     <input type="text" name="title" placeholder="Todoを入力">
     <button type="submit">追加</button>
   </form>
 
-
-
   <h2>一覧</h2>
+
   <ul>
-    <?php foreach ($todoList as $todo): ?>
+    <?php foreach ($todos as $todo): ?>
       <li>
         <?php echo htmlspecialchars($todo['title'], ENT_QUOTES, 'UTF-8'); ?>
+        <a href="?delete_id=<?php echo $todo['id']; ?>">削除</a>
       </li>
     <?php endforeach ?>
   </ul>
